9263216cfd8be830967c9b649ec306806848e94b
Maryan ibrahim <maryanibrahim301@yahoo.com>
2024-04-01 22:08:43 -0500

    php function

--- app/public/wp-content/themes/fictional-universty-theme/index.php
+++ app/public/wp-content/themes/fictional-universty-theme/index.php
@@ -1,1 +1,10 @@
-first theme
+<?php
+function greet($name, $color) {
+echo "<p> hello this is $name my favourite color is $color</p>";
+}
+
+greet('maryan', 'pink');
+greet('youssef', 'blue');
+?>
+<h1><?php bloginfo('name');?></h1>
+<h3><?php bloginfo('description');?></h3>
